77024e84eaaba8f93c3df9ee6e00db4e35a8703a
JoseMCR <68649485+josemcr008@users.noreply.github.com>
2022-09-28 12:23:47 +0200

    Add files via upload

---
+++ la1/Debug/imc/MultilayerPerceptron.d
@@ -0,0 +1,6 @@
+imc/MultilayerPerceptron.d: ../imc/MultilayerPerceptron.cpp \
+ ../imc/MultilayerPerceptron.h ../imc/util.h
+
+../imc/MultilayerPerceptron.h:
+
+../imc/util.h:
---
+++ la1/Debug/imc/util.d
@@ -0,0 +1,3 @@
+imc/util.d: ../imc/util.cpp ../imc/util.h
+
+../imc/util.h:
